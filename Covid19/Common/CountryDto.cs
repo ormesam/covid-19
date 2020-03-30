e135799c0c2d7d67aefa9efea6bbebbb94dfa884
Sam Orme <ormesam@hotmail.com>
2020-03-30 18:59:24 +0100

    Add overview details

--- Covid19/Common/CountryDto.cs
+++ Covid19/Common/CountryDto.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Common {
     public class CountryDto {
         public int CountryId { get; set; }
         public string Name { get; set; }
         public IList<RecordDto> Records { get; set; }
+        public RecordDto LastRecord => Records
+            .OrderByDescending(i => i.Date)
+            .FirstOrDefault();
 
         public CountryDto() {
             Records = new List<RecordDto>();
