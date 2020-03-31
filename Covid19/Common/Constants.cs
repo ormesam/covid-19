c9123285a1b65030d985efcc7f49ab93fe81767e
Sam Orme <ormesam@hotmail.com>
2020-03-31 19:46:46 +0100

    Add constants class to change api url depending on configuration

---
+++ Covid19/Common/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Common {
+    public static class Constants {
+#if DEBUG
+        public const string ApiUrl = "http://localhost:62702";
+#else
+        public const string ApiUrl = "https://covid-19-api.samorme.co.uk";
+#endif
+    }
+}
